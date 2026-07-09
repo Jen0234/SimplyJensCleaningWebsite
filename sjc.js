e5c8db9b00f90770d87101f0d010fb7be11968fe
Jenna Lafable <JennaLafable@gmail.com>
2026-07-09 00:06:34 -0400

    edit faq

--- sjc.js
+++ sjc.js
@@ -216,22 +216,25 @@ const revealOnScroll = () => {
 
 window.addEventListener("scroll", revealOnScroll);
 
-/* FAQ section of the code  */
+/* FAQ section */
 
-var acc = document.getElementsByClassName("faq");
-var i;
+document.addEventListener("DOMContentLoaded", function () {
+  const faqs = document.querySelectorAll(".faq");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var response = this.nextElementSibling;
-    if (response.style.maxHeight) {
-      response.style.maxHeight = null;
-    } else {
-      response.style.maxHeight = response.scrollHeight + "px";
-    } 
+  faqs.forEach(function (faq) {
+    faq.addEventListener("click", function () {
+      this.classList.toggle("active");
+
+      const response = this.nextElementSibling;
+
+      if (response.style.maxHeight) {
+        response.style.maxHeight = null;
+      } else {
+        response.style.maxHeight = response.scrollHeight + "px";
+      }
+    });
   });
-}
+});
 
 
 // footer of the code 
